2388ea73500b290a0ad4b46452729283973086e6
Nikola Kovacevic <nikola.kovacevic@hotmail.com>
2017-08-16 18:25:46 +0200

    Initial source_v4 upload

---
+++ source/Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    '..\\index.html': "index.html"
+                }
+            }
+        },
+        sass:{
+            dist: {
+                options: {
+                    style: 'compressed',
+                    sourceMap: false
+                },
+                files: {
+                    '..\\style.css': ['sass\\**\\*.sass']
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    '..\\app.js': ['javascript\\**\\*.js']
+                }
+            }
+        },
+
+        watch: {
+            html: {
+                files: ['index.html'],
+                tasks: ['htmlmin']
+            },
+            sass: {
+                files: ['sass\\**\\*.sass'],
+                tasks: ['sass']
+            },
+            js: {
+                files: ['javascript\\**\\*.js'],
+                tasks: ['uglify']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['htmlmin', 'sass', 'uglify', 'watch']);
+}
