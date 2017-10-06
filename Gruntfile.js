413abd6af9fe3eba96c571ec7bcd2c7f06ab8807
Nikola Kovacevic <nikola.kovacevic@hotmail.com>
2017-10-06 22:54:41 +0200

    Added basic grunt build config

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        clean: {
+            main: ['build/']
+        },
+        jade: {
+            main: { files: { 'build/index.html' : 'src/index.jade' } }
+        },
+        sass: {
+            main: { files: { 'build/style.css' : 'src/sass/00_main.sass'} }
+        },
+        autoprefixer: {
+            options: { browsers: ['last 2 versions', 'ie 8', 'ie 9'] },
+            main: { files: { 'build/style.css' : 'build/style.css' } }
+        },
+        cssmin: {
+            main: { files: { 'build/style.min.css' : 'build/style.css' } }
+        },
+        uglify: {
+            main: {
+                src: 'src/javascript/**/*.js',
+                dest: 'build/app.js'
+            }
+        },
+        copy: {
+            main: {
+                expand: true,
+                cwd: 'build',
+                src: '**',
+                dest: ''
+            }
+
+        }
+
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-sass');
+
+    grunt.registerTask('css', ['sass', 'autoprefixer', 'cssmin']);
+    grunt.registerTask('build', ['jade', 'css', 'uglify']);
+    grunt.registerTask('default', ['clean', 'build', 'copy']);
+};
