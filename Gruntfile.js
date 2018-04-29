feb0233067f5b9b6c4486f3db54558de4243a851
nkg5 <nikola.kovacevic@hotmail.com>
2018-04-29 15:06:39 +0200

    created main and build npm scripts, and changed sass compiler

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,7 +82,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-symlink');
